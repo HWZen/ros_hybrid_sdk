6a8a91e3fffe5d6283d2609f5d576eb50d2b1326
HWZen <1041453977@qq.com>
2022-12-15 13:47:10 +0800

    update: add Interface.h

---
+++ src/Interface.h
@@ -0,0 +1,40 @@
+//
+// Created by HWZen on 2022/12/13.
+// Copyright (c) 2022 HWZen All rights reserved.
+// MIT License
+// 
+
+#ifndef ROS_HYBRID_DYNAMIC_MSGS_INTERFACE_H
+#define ROS_HYBRID_DYNAMIC_MSGS_INTERFACE_H
+
+#include <string>
+#include <functional>
+
+namespace hybrid
+{
+    // return ros::Publisher and ros::Subscriber is better?
+    class MsgPublisher
+    {
+    public:
+        MsgPublisher(const std::string &topic, uint32_t queue_size, bool latch = false) {};
+
+        virtual void publish(const std::string &msg) = 0;
+
+        virtual ~MsgPublisher() = default;
+
+
+    };
+
+    class MsgSubscriber
+    {
+    public:
+        MsgSubscriber(const std::string &topic, uint32_t queue_size,
+                      const std::function<void(std::string)> &callback) {};
+
+        virtual ~MsgSubscriber() = default;
+
+
+    };
+}
+
+#endif //ROS_HYBRID_DYNAMIC_MSGS_INTERFACE_H
